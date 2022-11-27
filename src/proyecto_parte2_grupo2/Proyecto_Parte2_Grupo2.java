bcdb6edebebfb7cdbed8d7b17f398184730be956
Jesus Rodriguez <jesusr0604@unitec.edu>
2022-11-26 23:05:56 -0600

    Trabajen

---
+++ src/proyecto_parte2_grupo2/Proyecto_Parte2_Grupo2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_parte2_grupo2;
+
+/**
+ *
+ * @author larl2
+ */
+public class Proyecto_Parte2_Grupo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
